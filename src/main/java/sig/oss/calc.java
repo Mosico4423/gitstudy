38c5e5d7ac276aee07834664153f37860a3d2272
Mosico4423 <aktmxj1162@naver.com>
2016-08-24 15:51:38 +0900

    Update calc.java

--- src/main/java/sig/oss/calc.java
+++ src/main/java/sig/oss/calc.java
@@ -1,10 +1,18 @@
 public class Calc {
+  
+  public int r=0;
 public int add(int x, int y) { //add method
 return x+y;
 }
 pubilc int sub(int x int y){
 
 return x-y;
+
+public void det(int d) { //add emthod
+
+ r-=d
+
+}
 
 }
 
